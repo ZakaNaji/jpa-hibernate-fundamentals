33bc73c1c15a3adf61788bc1ad26b1275c9a5711
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-02-06 20:15:25 +0100

    #2: life cycle methods

--- src/main/java/com/znaji/App.java
+++ src/main/java/com/znaji/App.java
@@ -25,12 +25,29 @@ public class App
         try (EntityManager em = emf.createEntityManager()) {
             em.getTransaction().begin();
 
-            var product = new Product();
-            product.setId(4L);
-            product.setName("Bread And cheese");
-            em.persist(product);
+            //var product = new Product();
+            //product.setId(4L);
+            //product.setName("Bread And cheese");
+            //em.persist(product);// add it to the context and mark it for persistence(if we try to persiste with existing id will fail
 
-            em.getTransaction().commit();
+            var pr = em.find(Product.class, 5L);
+            System.out.println("Found product: " + pr);
+
+            var product = new Product();
+            product.setId(5L);
+            product.setName("Test");
+            var managed = em.merge(product);// adds product to context
+
+            em.refresh(managed);//refetch entity from db
+            System.out.println(managed);
+
+            //em.detach(managed); //remove if rom context
+
+            em.remove(managed); // mark it to be deleted
+
+            var other = em.getReference(Product.class, 1L); // return a proxy, will do DB req once the ref is used.
+
+                    em.getTransaction().commit();
         } catch (Exception e) {
             e.printStackTrace();
         }
