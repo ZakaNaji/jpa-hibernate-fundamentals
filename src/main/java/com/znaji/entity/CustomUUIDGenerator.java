e518dab139975c87df0e8292e642682ff17d7e69
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-02-08 15:04:35 +0100

    #3: custom id generation strategy: - add custom anno, and annotate the id with id - our custom anno, uses IdGeneratorType(generationStrategy.class) - generationStrategy class: implements BeforeExecutionGenerator interface: - define event type(inser, update,...) - generate: the generated id value

---
+++ src/main/java/com/znaji/entity/CustomUUIDGenerator.java
@@ -0,0 +1,14 @@
+package com.znaji.entity;
+
+import org.hibernate.annotations.IdGeneratorType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@IdGeneratorType(CustomUUIDGeneratorStrategy.class)
+public @interface CustomUUIDGenerator {
+}
---
+++ src/main/java/com/znaji/entity/CustomUUIDGeneratorStrategy.java
@@ -0,0 +1,24 @@
+package com.znaji.entity;
+
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.generator.BeforeExecutionGenerator;
+import org.hibernate.generator.EventType;
+import org.hibernate.generator.EventTypeSets;
+import org.hibernate.generator.Generator;
+
+import java.util.EnumSet;
+import java.util.UUID;
+
+public class CustomUUIDGeneratorStrategy implements BeforeExecutionGenerator {
+    @Override
+    public Object generate(SharedSessionContractImplementor session, Object owner, Object currentValue, EventType eventType) {
+        var uuid = UUID.randomUUID().toString();
+        System.out.println(uuid);
+        return uuid;
+    }
+
+    @Override
+    public EnumSet<EventType> getEventTypes() {
+        return EventTypeSets.INSERT_ONLY;
+    }
+}
